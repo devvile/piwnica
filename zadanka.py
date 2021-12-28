8a07fcb7884121fd19e9f415f0fd262f9f43539d
Dottore <patryk.czemierowski@gmail.com>
2021-12-28 23:02:42 +0100

    zadanka dodane

---
+++ zadanka.py
@@ -0,0 +1,23 @@
+''' 
+--------------- PROGRAM BAZA --------------
+Wymagania:
+Program na początku pyta uzytkownika o akcje, mozliwe akcje - add user, search user, exit
+Program po uruchomieniu ma być włączony dopóki uzytkownik nie wpisze komendy 'exit'
+Przy dodaniu uzytkownika, podajemy jego nazwe. Po czym program wraca do menu glownego.
+Przy wyszukaniu wpisujemy nazwe i program odpowiada czy uzytkownik juz jest w bazie.
+
+--------------- PROGRAM PALINDROM--------------
+
+Program ma za zadanie sprawdzac czy input od uzytkownika jest palindromem (czyta sie tak samo od przodu jak od tylu)
+
+--------------- PROGRAM CENZURUJACY--------------
+
+Program przyjmuje input od uzytkownika, po czym sprawdza czy tekst zawiera zakazane slowa,
+zamienia zakazane slowa z listy na ciag znakow typu "$@#$%" i oddaje ocenzurowany string.
+Przydatna funckja: split() 
+
+--------------- FizzBuzz--------------
+
+Program przyjmuje input od uzytkownika, po czym jesli input jest podzielny przez 2 wyswietla FIZZ, jesli przez 5 to wyswietla BUZZ, jesli przez 10 to FIZZBUZZ
+
+'''
